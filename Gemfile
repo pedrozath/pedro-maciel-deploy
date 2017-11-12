4cd95f5acba3b36053096e4ff4e79f1b41a3a810
Pedro Maciel <pedrozath@gmail.com>
2017-11-11 22:34:50 -0200

    first export

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'jekyll'
+gem 'activesupport'
+gem 'pry'
+gem 'mini_magick'
+
+group :jekyll_plugins do
+  gem 'jekyll-assets'
+  gem 'jekyll-coffeescript'
+  gem 'jekyll-livereload'
+  gem 'jekyll-haml', path: '../jekyll-haml'
+  gem 'jekyll-feed', '~> 0.6'
+end
+
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ index.js
@@ -0,0 +1,4 @@
+(function() {
+  $("main").html("<div class='contact-info'><div class='embedded-video'><iframe allowfullscreen class='motion-reel' frameborder='0' mozallowfullscreen src='https://player.vimeo.com/video/146427359?color=fdf16f&amp;byline=0&amp;portrait=0' webkitallowfullscreen></iframe></div><hr><h2 class='h2'>PEDRO@PEDROMACIEL.COM / +55 11 95252 8011</h2><ul class='inline'><li><a data-auto-route href='portfolio'>portfolio</a></li><li><a href='//br.linkedin.com/pub/pedro-maciel/22/752/a41' target='_blank'>LinkedIn</a></li></ul></div>");
+
+}).call(this);
